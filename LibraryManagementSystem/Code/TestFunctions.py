c13242e097bbe8400ef255e56f2874dc626c9e20
Adarsh Mallick <38970527+adaick@users.noreply.github.com>
2020-08-02 15:43:49 +0530

    Updated working of librarian and member.

--- LibraryManagementSystem/Code/TestFunctions.py
+++ LibraryManagementSystem/Code/TestFunctions.py
@@ -16,8 +16,39 @@ catalog.addBookItem(b, '123hg','H1B2')
 catalog.addBookItem(b, '124hg','H1B4')
 catalog.addBookItem(b, '125hg','H1B5')
 
-b = catalog.addBook('Moonwalking with Einstien','J Foer', '2017',318)
-catalog.addBookItem(b, '463hg','K1B2')
+c = catalog.addBook('Moonwalking with Einstien','J Foer', '2017',318)
+catalog.addBookItem(c, '463hg','K1B2')
+
+catalog.displayAllBooks()
+print("----------------------")
+
+m1 = Member(" Vish "," Bangalore ",23,' asljlkj22 ',' std1233 ')
+
+librarian = Librarian(" Awantik "," Bangalore ",34,' asljlkj22 ',' zeke101 ') 
+print (m1)
+print("----------------------")
+print (librarian)
+print("----------------------")
+# -*- coding: utf-8 -*-
+from Book import Book
+from Catalog import Catalog
+from User import Member, Librarian
+
+#b1 = Book('Shoe Dog','Phil Knight', '2015',312)
+#b1.addBookItem('123hg','H1B2')
+#b1.addBookItem('124hg','H1B3')
+
+#b1.printBook()
+
+catalog = Catalog()
+
+b = catalog.addBook('Shoe Dog','Phil Knight', '2015',312)
+catalog.addBookItem(b, '123hg','H1B2')
+catalog.addBookItem(b, '124hg','H1B4')
+catalog.addBookItem(b, '125hg','H1B5')
+
+c = catalog.addBook('Moonwalking with Einstien','J Foer', '2017',318)
+catalog.addBookItem(c, '463hg','K1B2')
 
 catalog.displayAllBooks()
 print("----------------------")
@@ -30,6 +61,32 @@ print("----------------------")
 print (librarian)
 print("----------------------")
 
+book = catalog.searchByName('Shoe Dog')
+print (book)
+print("----------------------")
+
+
+catalog.removeBookItem('Shoe Dog','124hg')
+catalog.displayAllBooks()
+print("----------------------")
+librarian.addBook(catalog,'Shoe Dog','Phil Knight', '2015',312)
+catalog.displayAllBooks()
+print("----------------------")
+librarian.removeBookItemFromCatalog(catalog,'Shoe Dog','125hg')
+catalog.displayAllBooks()
+print("----------------------")
+librarian.removeBook(c,'Moonwalking with Einstien')
+catalog.displayAllBooks()
+print("----------------------")
+librarian.addBookItem(catalog,c, '126hg','H1B6')
+catalog.displayAllBooks()
+print("----------------------")
+m1.issueBook(catalog,'Shoe Dog')
+catalog.displayAllBooks()
+print("----------------------")
+m1.returnBook(b,'Shoe Dog')
+catalog.displayAllBooks()
+print("----------------------")
 b = catalog.searchByName('Shoe Dog')
 print (b)
 print("----------------------")
@@ -41,7 +98,13 @@ print("----------------------")
 librarian.addBook(catalog,'Shoe Dog','Phil Knight', '2015',312)
 catalog.displayAllBooks()
 print("----------------------")
-librarian.removeBook('Shoe Dog')
+librarian.removeBookItemFromCatalog(catalog,'Shoe Dog','125hg')
 catalog.displayAllBooks()
 print("----------------------")
-#m1.issueBook
+librarian.removeBook(c,'Moonwalking with Einstien')
+catalog.displayAllBooks()
+print("----------------------")
+librarian.addBookItem(catalog,c, '126hg','H1B6')
+catalog.displayAllBooks()
+print("----------------------")
+m1.issueBook(catalog,'Shoe Dog')
--- LibraryManagementSystem/Code/User.py
+++ LibraryManagementSystem/Code/User.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from Catalog import Catalog
 from Book import Book
+
 class User:
     def __init__(self, name, location, age, aadhar_id):
         self.name = name
@@ -10,20 +11,47 @@ class User:
         
 
 class Member(User):
+    members_list = []
+
+    @classmethod
+    def addMemberList(cls, member):
+        cls.members_list.append(member)
+
     def __init__(self,name, location, age, aadhar_id,student_id):
         super().__init__(name, location, age, aadhar_id)
         self.student_id = student_id
+        self.issuedbook = []
+        Member.addMemberList(self)
         
     def __repr__(self):
         return self.name + ' ' + self.location + ' ' + self.student_id
     
     #assume name is unique
-    def issueBook(self,name,days=10):
-        pass
+    def issueBook(self,obj, name,days=10):
+        m_name=self.name
+        try:
+            for book in obj.books:
+                if book.name==name:
+                    isbn=book.book_item[0].isbn
+                    rack=book.book_item[0].rack
+            isssued_item=obj.removeBookItem(name,isbn)
+            self.issuedbook.extend((name,isbn,rack))
+            print('{} successfully Issued'.format(name))
+        except  IndexError:
+            print('{} Not available'.format(name))
     
     #assume name is unique
-    def returnBook(self,name):
-        pass
+    def returnBook(self,obj,name):
+        try:
+            if name in self.issuedbook:
+                isbn=self.issuedbook[1]
+                rack=self.issuedbook[2]
+            obj.addBookItem(isbn,rack)
+            print('{} successfully returned'.format(name))
+        except UnboundLocalError:
+            print('{} is not issued'.format(name))
+
+
         
         
 class Librarian(User):
@@ -36,10 +64,12 @@ class Librarian(User):
     
     def addBook(self,catalog,name,author,publish_date,pages):
         catalog.addBook(name,author,publish_date,pages)
+
+    def addBookItem(self,catalog,book,isbn,rack):
+        catalog.addBookItem(book,isbn, rack)
     
-    def removeBook(self,name):
-        book_item=name
-        Book.removeBookItem(book_item)
+    def removeBook(self,book,name):
+        book.removeBookItem(name)
     
     def removeBookItemFromCatalog(self,catalog,book,isbn):
         name = book

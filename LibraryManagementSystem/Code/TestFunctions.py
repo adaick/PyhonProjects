e4d887be51e85863bd45cb1d8c91e2d2e550cf3b
Adarsh Mallick <38970527+adaick@users.noreply.github.com>
2020-08-02 15:45:34 +0530

    updated

--- LibraryManagementSystem/Code/TestFunctions.py
+++ LibraryManagementSystem/Code/TestFunctions.py
@@ -29,37 +29,6 @@ print (m1)
 print("----------------------")
 print (librarian)
 print("----------------------")
-# -*- coding: utf-8 -*-
-from Book import Book
-from Catalog import Catalog
-from User import Member, Librarian
-
-#b1 = Book('Shoe Dog','Phil Knight', '2015',312)
-#b1.addBookItem('123hg','H1B2')
-#b1.addBookItem('124hg','H1B3')
-
-#b1.printBook()
-
-catalog = Catalog()
-
-b = catalog.addBook('Shoe Dog','Phil Knight', '2015',312)
-catalog.addBookItem(b, '123hg','H1B2')
-catalog.addBookItem(b, '124hg','H1B4')
-catalog.addBookItem(b, '125hg','H1B5')
-
-c = catalog.addBook('Moonwalking with Einstien','J Foer', '2017',318)
-catalog.addBookItem(c, '463hg','K1B2')
-
-catalog.displayAllBooks()
-print("----------------------")
-
-m1 = Member(" Vish "," Bangalore ",23,' asljlkj22 ',' std1233 ')
-
-librarian = Librarian(" Awantik "," Bangalore ",34,' asljlkj22 ',' zeke101 ') 
-print (m1)
-print("----------------------")
-print (librarian)
-print("----------------------")
 
 book = catalog.searchByName('Shoe Dog')
 print (book)
